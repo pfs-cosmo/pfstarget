93c53f4427505b618edb77a9cda4700cbda9c6ef
ChangHoon Hahn <chhahn@arizona.edu>
2025-03-04 12:17:37 -0500

    - added masking cuts (halo, ghost, blooming) - added quality cuts to remove objects without g, r, i, z magnitude - added quality color cuts (g-r > -1 and i-z > -1)

--- src/pfstarget/cuts.py
+++ src/pfstarget/cuts.py
@@ -24,6 +24,8 @@ def isCosmology(objects, star_galaxy_cut=-0.15, magnitude_cut=22.5,
     * DESI elg target selection https://github.com/desihub/desitarget/blob/main/py/desitarget/cuts.py#L646
 
     '''
+    # mask
+    is_mask = masking(objects) 
 
     # quality cuts
     is_quality = quality_cuts(objects)
@@ -34,7 +36,7 @@ def isCosmology(objects, star_galaxy_cut=-0.15, magnitude_cut=22.5,
     # color cut
     is_color = color_cut(objects, magnitude_cut=magnitude_cut) 
 
-    return is_quality & is_galaxy & is_color 
+    return ~is_mask & is_quality & is_galaxy & is_color 
 
 
 def color_cut(objects, magnitude_cut=22.5, g_r_cut=0.15, color_slope=2.0, color_yint=-0.15): 
@@ -46,7 +48,7 @@ def color_cut(objects, magnitude_cut=22.5, g_r_cut=0.15, color_slope=2.0, color_
     
     # color cut 
     cuts &= (((objects['G_MAG'] - objects['R_MAG']) < 0.15) | # g-r cut (for 1.6 < z < 2.4 ELGs) 
-             ((objects['I_MAG'] - objects['Y_MAG']) 
+             ((objects['I_MAG'] - objects['Z_MAG']) 
               > color_slope * (objects['G_MAG'] - objects['R_MAG'])- color_yint))
     return cuts 
 
@@ -55,24 +57,45 @@ def quality_cuts(objects):
     ''' quality cuts. Currently we only impose a magnitude depend cut on g-band
     magnitude error.
     '''
-    # g-band magnitude error cut
-    cuts = (objects['G_ERR'] < objects['G_MAG'] * 0.05 - 1.1) 
+    # has g, r, i, y-band magnitudes 
+    cuts = (np.isfinite(objects['G_MAG']) & 
+            np.isfinite(objects['R_MAG']) & 
+            np.isfinite(objects['I_MAG']) & 
+            np.isfinite(objects['Z_MAG']))
 
-    # TODO: extreme colors
-    # There should probably be a cut on extreme colors 
+    # g-band magnitude error cut
+    cuts &= (objects['G_ERR'] < objects['G_MAG'] * 0.05 - 1.1) 
+
+    # cut on extreme colors (these values are preliminary --- revisit this in detail)
+    cuts &= ((objects['G_MAG'] - objects['R_MAG'] > -1) & 
+             (objects['I_MAG'] - objects['Z_MAG'] > -1))
     return cuts
 
 
-def star_galaxy(i_cmodel, i_psf, cut=-0.15): 
+def star_galaxy(objects, cut=-0.15): 
     ''' star-galaxy separation. Currently PSF star-galaxy separation is:
 
-    [i-band cmodel magnitude] - [i-band PSF magnitude] < cut
+    [measured i-band cmodel magnitude] - [measured i-band PSF magnitude] < cut
 
     This cut essentially determines whether the object is extended and has
     significant light outside of the PSF profile. 
     '''
-    return (i_cmodel - i_psf < cut)
+    return (objects['I_MEAS_CMODEL_MAG'] - objects['I_MEAS_PSF_MAG'] < cut)
 
+
+def masking(objects): 
+    ''' masks including mask around bright objects from ghost, halo, blooming
+
+    see https://hscla.mtk.nao.ac.jp/doc/wp-content/uploads/2020/12/brightstarmask.pdf
+    for additional details on the masking. 
+
+    return: 
+        boolean array that specifies the objects that are *within* the mask 
+    '''
+    _mask = objects['I_MASK_HALO'].astype(bool) # within bright star halo 
+    _mask &= objects['I_MASK_GHOST'].astype(bool) # larger circular radius around the bright star 
+    _mask &= objects['I_MASK_BLOOMING'].astype(bool) 
+    return _mask 
 
 def _prepare_hsc(hsc, dust_extinction='default'): 
     ''' prepare HSC imaging data for target selection 
@@ -97,8 +120,11 @@ def _prepare_hsc(hsc, dust_extinction='default'):
              ('I_ERR', 'f4'), 
              ('Z_ERR', 'f4'), 
              ('Y_ERR', 'f4'), 
-             #('I_MEAS_CMODEL_MAG', 'f4'), 
-             ('I_PSF_MAG', 'f4')
+             ('I_MEAS_CMODEL_MAG', 'f4'), 
+             ('I_MEAS_PSF_MAG', 'f4'), 
+             ('I_MASK_HALO', 'i'), 
+             ('I_MASK_GHOST', 'i'), 
+             ('I_MASK_BLOOMING', 'i')
              ]
 
     objects = np.zeros(len(hsc), dtype=dtype)
@@ -137,7 +163,13 @@ def _prepare_hsc(hsc, dust_extinction='default'):
 
     # i-band measured CMODEL and PSF magnitudes for star-galaxy separation 
     objects['I_MEAS_CMODEL_MAG'] = hsc["i_meas_cmodel_mag"]
-    objects['I_MEAS_PSF_MAG'] = hsc["i_meas_psf_mag"]
+    # TEMPORARILY INCLUDING TYPO FIX THIS BACK TO PSF LATER
+    objects['I_MEAS_PSF_MAG'] = hsc["i_meas_pfs_mag"] 
+
+    # i-band mask for bright stars
+    objects['I_MASK_HALO']      = hsc["i_mask_brightstar_halo"].astype(int)
+    objects['I_MASK_GHOST']     = hsc["i_mask_brightstar_ghost"].astype(int)
+    objects['I_MASK_BLOOMING']  = hsc["i_mask_brightstar_blooming"].astype(int)
 
     # additional columns 
     objects['OBJID'] = hsc['object_id']  # object id 
