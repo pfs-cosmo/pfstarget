f60117e86bf4756b7d7e3e29b4da615183d545a3
ChangHoon Hahn <chhahn@arizona.edu>
2025-03-27 10:15:21 -0400

    masking for randoms implemented: consistent as the masking for imaging

--- src/pfstarget/cuts.py
+++ src/pfstarget/cuts.py
@@ -107,102 +107,127 @@ def masking(objects):
     return: 
         boolean array that specifies the objects that are *within* the mask 
     '''
-    _mask = objects['I_MASK_HALO'].astype(bool) # within bright star halo 
-    _mask &= objects['I_MASK_GHOST'].astype(bool) # larger circular radius around the bright star 
-    _mask &= objects['I_MASK_BLOOMING'].astype(bool) 
+    _mask = objects['i_mask_halo'].astype(bool) # within bright star halo 
+    _mask &= objects['i_mask_ghost'].astype(bool) # larger circular radius around the bright star 
+    _mask &= objects['i_mask_blooming'].astype(bool) 
     return _mask 
 
 
-def _prepare_hsc(hsc, dust_extinction='sfd98', release='s23b', zeropoint=True): 
-    ''' prepare HSC imaging data for target selection 
+def _prepare_hsc(hsc, dust_extinction='sfd98', release='s23b', zeropoint=true): 
+    ''' prepare hsc imaging data for target selection 
 
     args:
         hsc : object 
-            astropy.Table or some structured array with hsc data 
+            astropy.table or some structured array with hsc data 
 
         dust_extinction : str
             string specifying the galactic dust extinction model 
             (default: 'sfd98) 
 
         release : str
-            string specifying the HSC data release (Default: s23b) 
+            string specifying the hsc data release (default: s23b) 
     
     return: 
-        objects: structured numpy array of HSC objects with relevant columns
+        objects: structured numpy array of hsc objects with relevant columns
         for target selection 
     '''
-    dtype = [('OBJID', '<i8'), 
-             ('RA', 'f4'), 
-             ('DEC', 'f4'), 
-             ('G_MAG', 'f4'), 
-             ('R_MAG', 'f4'), 
-             ('I_MAG', 'f4'), 
-             ('Z_MAG', 'f4'), 
-             ('Y_MAG', 'f4'), 
-             ('G_ERR', 'f4'), 
-             ('R_ERR', 'f4'), 
-             ('I_ERR', 'f4'), 
-             ('Z_ERR', 'f4'), 
-             ('Y_ERR', 'f4'), 
-             ('I_MEAS_CMODEL_MAG', 'f4'), 
-             ('I_MEAS_PSF_MAG', 'f4'), 
-             ('I_MASK_HALO', 'i'), 
-             ('I_MASK_GHOST', 'i'), 
-             ('I_MASK_BLOOMING', 'i'),
-             ('G_PSF_FLAG', bool), 
-             ('R_PSF_FLAG', bool),
-             ('I_PSF_FLAG', bool), 
-             ('Z_PSF_FLAG', bool), 
-             ('DEBLEND_SKIPPED', bool), 
-             ('I_APFLUX10_MAG', 'f4'),  
-             ('I_APFLUX10_FLAG', bool)
+    dtype = [('objid', '<i8'), 
+             ('ra', 'f4'), 
+             ('dec', 'f4'), 
+             ('g_mag', 'f4'), 
+             ('r_mag', 'f4'), 
+             ('i_mag', 'f4'), 
+             ('z_mag', 'f4'), 
+             ('y_mag', 'f4'), 
+             ('g_err', 'f4'), 
+             ('r_err', 'f4'), 
+             ('i_err', 'f4'), 
+             ('z_err', 'f4'), 
+             ('y_err', 'f4'), 
+             ('i_meas_cmodel_mag', 'f4'), 
+             ('i_meas_psf_mag', 'f4'), 
+             ('i_mask_halo', 'i'), 
+             ('i_mask_ghost', 'i'), 
+             ('i_mask_blooming', 'i'),
+             ('g_psf_flag', bool), 
+             ('r_psf_flag', bool),
+             ('i_psf_flag', bool), 
+             ('z_psf_flag', bool), 
+             ('deblend_skipped', bool), 
+             ('i_apflux10_mag', 'f4'),  
+             ('i_apflux10_flag', bool)
              ]
 
     objects = np.zeros(len(hsc), dtype=dtype)
 
     # grizy magnitudes corrections for galactic dust extinction 
-    _g, _r, _i, _z, _y = E._extinction_correct(hsc, method=dust_extinction, 
+    _g, _r, _i, _z, _y = e._extinction_correct(hsc, method=dust_extinction, 
                                                release=release,
                                                zeropoint=zeropoint)
-    objects['G_MAG'] = _g
-    objects['R_MAG'] = _r
-    objects['I_MAG'] = _i
-    objects['Z_MAG'] = _z
-    objects['Y_MAG'] = _y
+    objects['g_mag'] = _g
+    objects['r_mag'] = _r
+    objects['i_mag'] = _i
+    objects['z_mag'] = _z
+    objects['y_mag'] = _y
 
-    # g-band CMODEL magnitude used for quality cuts 
-    objects['G_ERR'] = hsc["g_cmodel_mag_err"]
-    objects['R_ERR'] = hsc["r_cmodel_mag_err"]
-    objects['I_ERR'] = hsc["i_cmodel_mag_err"]
-    objects['Z_ERR'] = hsc["z_cmodel_mag_err"]
-    objects['Y_ERR'] = hsc["y_cmodel_mag_err"]
+    # g-band cmodel magnitude used for quality cuts 
+    objects['g_err'] = hsc["g_cmodel_mag_err"]
+    objects['r_err'] = hsc["r_cmodel_mag_err"]
+    objects['i_err'] = hsc["i_cmodel_mag_err"]
+    objects['z_err'] = hsc["z_cmodel_mag_err"]
+    objects['y_err'] = hsc["y_cmodel_mag_err"]
 
-    # i-band measured CMODEL and PSF magnitudes for star-galaxy separation 
-    objects['I_MEAS_CMODEL_MAG'] = hsc["i_meas_cmodel_mag"]
-    # TEMPORARILY INCLUDING TYPO FIX THIS BACK TO PSF LATER
-    objects['I_MEAS_PSF_MAG'] = hsc["i_meas_psf_mag"] 
+    # i-band measured cmodel and psf magnitudes for star-galaxy separation 
+    objects['i_meas_cmodel_mag'] = hsc["i_meas_cmodel_mag"]
+    # temporarily including typo fix this back to psf later
+    objects['i_meas_psf_mag'] = hsc["i_meas_psf_mag"] 
 
     # i-band mask for bright stars
-    objects['I_MASK_HALO']      = hsc["i_mask_brightstar_halo"].astype(int)
-    objects['I_MASK_GHOST']     = hsc["i_mask_brightstar_ghost"].astype(int)
-    objects['I_MASK_BLOOMING']  = hsc["i_mask_brightstar_blooming"].astype(int)
+    objects['i_mask_halo']      = hsc["i_mask_brightstar_halo"].astype(int)
+    objects['i_mask_ghost']     = hsc["i_mask_brightstar_ghost"].astype(int)
+    objects['i_mask_blooming']  = hsc["i_mask_brightstar_blooming"].astype(int)
     
-    # PSF flag used for quality cut 
-    objects['G_PSF_FLAG'] = hsc['g_psf_flag']
-    objects['R_PSF_FLAG'] = hsc['r_psf_flag']
-    objects['I_PSF_FLAG'] = hsc['i_psf_flag']
-    objects['Z_PSF_FLAG'] = hsc['z_psf_flag']
+    # psf flag used for quality cut 
+    objects['g_psf_flag'] = hsc['g_psf_flag']
+    objects['r_psf_flag'] = hsc['r_psf_flag']
+    objects['i_psf_flag'] = hsc['i_psf_flag']
+    objects['z_psf_flag'] = hsc['z_psf_flag']
     
     # skipped by deblender 
-    objects['DEBLEND_SKIPPED'] = hsc['deblend_skipped']
+    objects['deblend_skipped'] = hsc['deblend_skipped']
 
     # aperture flux 
-    objects['I_APFLUX10_MAG']    = hsc['i_apertureflux_10_mag']
-    objects['I_APFLUX10_FLAG']   = hsc['i_apertureflux_10_flag']
+    objects['i_apflux10_mag']    = hsc['i_apertureflux_10_mag']
+    objects['i_apflux10_flag']   = hsc['i_apertureflux_10_flag']
 
     # additional columns 
-    objects['OBJID'] = hsc['object_id']  # object id 
-    objects['RA'] = hsc['ra'] # RA from i-band measurement
-    objects['DEC'] = hsc['dec'] # RA from i-band measurement
+    objects['objid'] = hsc['object_id']  # object id 
+    objects['ra'] = hsc['ra'] # ra from i-band measurement
+    objects['dec'] = hsc['dec'] # ra from i-band measurement
              
     return objects
+
+
+def random_masking(randoms): 
+    ''' masks including mask around bright objects from ghost, halo, blooming
+    for random catalog 
+
+    see https://hscla.mtk.nao.ac.jp/doc/wp-content/uploads/2020/12/brightstarmask.pdf
+    for additional details on the masking. 
+
+    return: 
+        boolean array that specifies the objects that are *within* the mask 
+    '''
+    # same inputcount masks implemented for the imaging (this is implemented in
+    # the SQL script) 
+    _mask = (randoms['g_inputcount_value'] >= 4)
+    _mask &= (randoms['r_inputcount_value'] >= 4)
+    _mask &= (randoms['i_inputcount_value'] >= 5)
+    _mask &= (randoms['z_inputcount_value'] >= 5)
+
+    # same brightstar masks implemented for the imaging 
+    _mask &= randoms['i_mask_brightstar_halo']
+    _mask &= randoms['i_mask_brightstar_ghost']
+    _mask &= randoms['i_mask_brightstar_blooming']
+
+    return _mask 
